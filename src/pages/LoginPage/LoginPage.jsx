1176c222a8d0299162c7d260106518edb98ed22a
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-11-20 18:00:22 +0200

    Update login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LoginPage.module.css";
 import Button from "../../utils/Button/Button.jsx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../../api/userApi";
+import { Hourglass } from "react-loader-spinner";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const loginSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const response = await loginUser({ email, password });
+      console.log(response);
+      const token = response.data;
+      localStorage.setItem("token", token);
+
+      if (!response) {
+        throw new Error("Login failed");
+      }
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form action="login" className={styles.labelFormContainer}>
+      <form
+        action="login"
+        onSubmit={loginSubmit}
+        className={styles.labelFormContainer}
+      >
+        {isLoading && <Hourglass visible={true} height="80" width="80" />}
         <h1 className={styles.labelFormTitle}>Log in</h1>
         <label htmlFor="login" className={styles.labelForm}>
-          <input name="email" id="email" type="text" placeholder="Email" />
+          <input
+            name="email"
+            id="email"
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             type="password"
             name="password"
             id="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <Button label="Log in" type="submit" className={styles.loginButton} />
