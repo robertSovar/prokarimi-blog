d6d86257f0f1fdd0239f5f2da36b83e7a9bfb4c3
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-12-01 10:36:12 +0200

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://prokarimi-blog-backend.onrender.com/api",
+  baseURL:
+    "https://prokarimi-blog-backend-k5lnn50uy-robertsovars-projects.vercel.app",
 });
